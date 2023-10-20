50e48fe0fb7b09c07a2e006fb6808a551f8718ad
vanhungsi <127096791+vanhungsi@users.noreply.github.com>
2023-10-20 22:11:45 +0700

    Initial

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todos.txt"
+def get_todo(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("hello from functions")
